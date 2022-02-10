a86483ec954fefa0e537cc1e9e85640ef6905b16
justinmsuen <jsx616@gmail.com>
2022-02-10 18:02:27 -0500

    new changes feb 10

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const {destinations: luxi} = require("./db");
+const {destinations} = require("./db");
 const express = require("express");
 
-const PORT = 3000;
+//const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
+
 const server = express();
 server.listen(PORT, () => {
     console.log(`Server listening on PORT: ${PORT}`);
 });
 
 server.get('/', (req, res) => {
-    res.send(luxi);
+    res.send(destinations);
 });
